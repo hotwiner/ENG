b3f0544ac7d5b0b4accfaa9c2ce7d67babcfd56d
kayraoguz21 <kayraoguz21@gmail.com>
2022-09-26 20:15:41 +0300

    env.sh updated

--- build/env.sh
+++ build/env.sh
@@ -1,1 +1,6 @@
+rm -f ../setup_info.h
+cat <<EOF >../setup_info.h
+#define ROOT "@PROJECT_SOURCE_DIR@"
+#define DEBUG true
+EOF
 cmake -GNinja  ./..
--- setup_info.h
+++
@@ -1,3 +0,0 @@
-//PROJECT_SOURCE_DIR
-#define ROOT "/home/nimbus/CODE/PROJECTS/ENG"
-#define DEBUG true
